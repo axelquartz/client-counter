09a31f6f92935d1dc1fd09c3deb6506af8430b8b
Axel Robles <axelarep@gmail.com>
2023-04-10 18:28:53 -0600

    Add structure and declare variables

---
+++ script.js
@@ -0,0 +1,4 @@
+let counter = document.getElementById('counter');
+let incrementBtn = document.getElementById('increment-btn');
+let saveBtn = document.getElementById('save-btn');
+let previousEntries = document.getElementById('previous-entries')
