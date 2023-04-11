6b459cc9c3150abc7ff1e6769e6ec66878c6efef
Axel Robles <axelarep@gmail.com>
2023-04-10 18:58:57 -0600

    Counter is adding numbers

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let counter = document.getElementById('counter');
+let displayCount = document.getElementById('display-count');
+let counter = 0;
 let incrementBtn = document.getElementById('increment-btn');
 let saveBtn = document.getElementById('save-btn');
 let previousEntries = document.getElementById('previous-entries')
+
+//Increase counter by one
+function addNumber() {
+    counter += 1;
+    displayCount.innerText = counter;
+}
